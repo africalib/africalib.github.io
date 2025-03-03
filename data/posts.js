d4f1dbf57655b4eec8132c408c304e64c33fb5ba
africalib <africalibrary21@gmail.com>
2025-03-03 18:26:20 +0900

    published

--- data/posts.js
+++ data/posts.js
@@ -1,7 +1,7 @@
 window.posts = [
     {
-        id: 25030302,
-        thumb: "/data/posts/25030302/img/undraw_ship-it_vn4g.svg",
+        id: 3,
+        thumb: "/data/posts/3/img/undraw_ship-it_vn4g.svg",
         title: "소프트웨어 관점으로 보는 서버와 클라이언트",
         desc: "통신을 할 때 역할에 따라 나눠지는 클라이언트와 서버에 대해 알아봅니다.",
         content: null,
@@ -9,8 +9,8 @@ window.posts = [
         tags: ["basic"],
     },
     {
-        id: 25030301,
-        thumb: "/data/posts/25030301/img/undraw_server-cluster_7ugi.svg",
+        id: 2,
+        thumb: "/data/posts/2/img/undraw_server-cluster_7ugi.svg",
         title: "하드웨어 관점으로 보는 서버와 클라이언트",
         desc: "통신을 할 때 역할에 따라 나눠지는 클라이언트와 서버에 대해 알아봅니다.",
         content: null,
@@ -18,8 +18,8 @@ window.posts = [
         tags: ["basic"],
     },
     {
-        id: 25030201,
-        thumb: "/data/posts/25030201/img/undraw_connected-world_anke.svg",
+        id: 1,
+        thumb: "/data/posts/1/img/undraw_connected-world_anke.svg",
         title: "파일을 주고받는 시스템, 인터넷",
         desc: "통신을 할 때 역할에 따라 나눠지는 클라이언트와 서버에 대해 알아봅니다.",
         content: null,
