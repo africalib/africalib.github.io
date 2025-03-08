e0586844b0c5fc1da0f2af480c7a0dfa21d60f91
africalib <africalibrary21@gmail.com>
2025-03-08 14:30:32 +0900

    published

--- assets/1-tVyY97VD.js
+++ assets/1-tVyY97VD.js
@@ -1,4 +1,4 @@
-import{a as l,d as t,F as o,o as p}from"./index-CNrK2Ilf.js";const d={},T=`<p>파일을 주고받는 시스템은 인터넷을 통해 데이터를 전송하는 방식으로, 개인 사용자부터 기업 환경까지 다양한 방식으로 활용됩니다. 이 글에서는 파일 전송 방식과 보안 고려 사항을 살펴봅니다.</p>
+import{a as l,d as t,F as o,o as p}from"./index-CMzYf-MW.js";const d={},T=`<p>파일을 주고받는 시스템은 인터넷을 통해 데이터를 전송하는 방식으로, 개인 사용자부터 기업 환경까지 다양한 방식으로 활용됩니다. 이 글에서는 파일 전송 방식과 보안 고려 사항을 살펴봅니다.</p>
 <ul>
 <li>HTTP/HTTPS: 웹 브라우저를 이용해 파일을 업로드/다운로드할 때 사용됨.</li>
 <li>FTP/SFTP: 파일 전송을 위한 프로토콜로, FTP는 암호화되지 않으며, SFTP는 SSH를 이용해 보안을 강화함.</li>
