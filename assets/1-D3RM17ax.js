70eab62eb3f3aabda9621909e3277c8f8c2d6c8e
africalib <africalibrary21@gmail.com>
2025-03-08 14:10:27 +0900

    published

---
+++ assets/1-D3RM17ax.js
@@ -0,0 +1,38 @@
+import{a as l,d as t,F as o,o as p}from"./index-CRSKRMDR.js";const d={},T=`<p>파일을 주고받는 시스템은 인터넷을 통해 데이터를 전송하는 방식으로, 개인 사용자부터 기업 환경까지 다양한 방식으로 활용됩니다. 이 글에서는 파일 전송 방식과 보안 고려 사항을 살펴봅니다.</p>
+<ul>
+<li>HTTP/HTTPS: 웹 브라우저를 이용해 파일을 업로드/다운로드할 때 사용됨.</li>
+<li>FTP/SFTP: 파일 전송을 위한 프로토콜로, FTP는 암호화되지 않으며, SFTP는 SSH를 이용해 보안을 강화함.</li>
+</ul>
+<pre><code class="language-js">window.alert('hello')
+</code></pre>
+<pre><code>&lt;script setup&gt;
+&lt;/script&gt;
+
+&lt;template&gt;
+    &lt;div&gt;&lt;/div&gt;
+&lt;/template&gt;
+</code></pre>
+`,a=`파일을 주고받는 시스템은 인터넷을 통해 데이터를 전송하는 방식으로, 개인 사용자부터 기업 환경까지 다양한 방식으로 활용됩니다. 이 글에서는 파일 전송 방식과 보안 고려 사항을 살펴봅니다.  \r
+\r
+- HTTP/HTTPS: 웹 브라우저를 이용해 파일을 업로드/다운로드할 때 사용됨.\r
+- FTP/SFTP: 파일 전송을 위한 프로토콜로, FTP는 암호화되지 않으며, SFTP는 SSH를 이용해 보안을 강화함.\r
+\r
+\`\`\`js\r
+window.alert('hello')\r
+\`\`\`\r
+\r
+\`\`\`\r
+<script setup>\r
+<\/script>\r
+\r
+<template>\r
+    <div></div>\r
+</template>\r
+\`\`\``;function r(e,n){return p(),l(o,null,[n[0]||(n[0]=t("p",null,"파일을 주고받는 시스템은 인터넷을 통해 데이터를 전송하는 방식으로, 개인 사용자부터 기업 환경까지 다양한 방식으로 활용됩니다. 이 글에서는 파일 전송 방식과 보안 고려 사항을 살펴봅니다.",-1)),n[1]||(n[1]=t("ul",null,[t("li",null,"HTTP/HTTPS: 웹 브라우저를 이용해 파일을 업로드/다운로드할 때 사용됨."),t("li",null,"FTP/SFTP: 파일 전송을 위한 프로토콜로, FTP는 암호화되지 않으며, SFTP는 SSH를 이용해 보안을 강화함.")],-1)),n[2]||(n[2]=t("pre",null,[t("code",{class:"language-js","v-pre":"true"},`window.alert('hello')
+`)],-1)),n[3]||(n[3]=t("pre",null,[t("code",{"v-pre":"true"},`<script setup>
+<\/script>
+
+<template>
+    <div></div>
+</template>
+`)],-1))],64)}const s={render:r};s.__hmrId="C:/J/repositories/africalib/src/data/posts/1.md";const u=e=>({components:e,render:r});export{s as VueComponent,u as VueComponentWith,d as attributes,T as html,a as markdown};
