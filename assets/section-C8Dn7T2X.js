9184fe7e02ba92d9564c0b0aa3b36c9b1f6c20cf
africalib <africalibrary21@gmail.com>
2025-07-24 15:33:09 +0900

    published

---
+++ assets/section-C8Dn7T2X.js
@@ -0,0 +1,1 @@
+import{s as r,l as o}from"./index-BSr5jZqr.js";const l=r("section",()=>{const e=o(!1);return{preventScroll:e,setPreventScroll:t=>{e.value=t}}});export{l as u};
--- assets/vqlmv-Dzl8xNDD.js
+++ assets/vqlmv-Dzl8xNDD.js
@@ -1,2 +1,2 @@
-import{c as n,o}from"./index-DQ7ziFAZ.js";const a={},m=`<p>학습 내용과 최종 목표</p>
+import{c as n,o}from"./index-BSr5jZqr.js";const a={},m=`<p>학습 내용과 최종 목표</p>
 `,i="학습 내용과 최종 목표";function t(e,c){return o(),n("p",null,"학습 내용과 최종 목표")}const r={render:t};r.__hmrId="C:/J/repositories/africalib/src/data/sections/vqlmv.md";const p=e=>({components:e,render:t});export{r as VueComponent,p as VueComponentWith,a as attributes,m as html,i as markdown};
