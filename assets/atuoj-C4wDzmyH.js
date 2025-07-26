c7af0cca2ae5a8f09e344b67ce7dfa07b8fb6ff6
africalib <africalibrary21@gmail.com>
2025-07-27 01:48:33 +0900

    published

--- assets/atuoj-C4wDzmyH.js
+++
@@ -1,8 +0,0 @@
-import{c as e,o as r,b as s,F as l}from"./index-DhUjy1vJ.js";const u={},i=`<p><img src="/assets/sections/atuoj/hello.svg" alt="인사"></p>
-<p>안녕하세요? 아프리카에 도서관을 짓는 것이 꿈인 개발자 최진입니다. 유튜브의 &lt;아프리카도서관&gt; 채널을 통해 프로그래밍과 관련된 내용을 공유하고 있습니다.</p>
-<p>안녕하세요? 아프리카에 도서관을 짓는 것이 꿈인 개발자 최진입니다. 유튜브의 &lt;아프리카도서관&gt; 채널을 통해 프로그래밍과 관련된 내용을 공유하고 있습니다.</p>
-`,m=`![인사](/assets/sections/atuoj/hello.svg)\r
-\r
-안녕하세요? 아프리카에 도서관을 짓는 것이 꿈인 개발자 최진입니다. 유튜브의 <아프리카도서관> 채널을 통해 프로그래밍과 관련된 내용을 공유하고 있습니다.  \r
-  \r
-안녕하세요? 아프리카에 도서관을 짓는 것이 꿈인 개발자 최진입니다. 유튜브의 <아프리카도서관> 채널을 통해 프로그래밍과 관련된 내용을 공유하고 있습니다.`;function o(n,t){return r(),e(l,null,[t[0]||(t[0]=s("p",null,[s("img",{src:"/assets/sections/atuoj/hello.svg",alt:"인사"})],-1)),t[1]||(t[1]=s("p",null,"안녕하세요? 아프리카에 도서관을 짓는 것이 꿈인 개발자 최진입니다. 유튜브의 <아프리카도서관> 채널을 통해 프로그래밍과 관련된 내용을 공유하고 있습니다.",-1)),t[2]||(t[2]=s("p",null,"안녕하세요? 아프리카에 도서관을 짓는 것이 꿈인 개발자 최진입니다. 유튜브의 <아프리카도서관> 채널을 통해 프로그래밍과 관련된 내용을 공유하고 있습니다.",-1))],64)}const a={render:o};a.__hmrId="C:/J/repositories/africalib/src/data/courses/qkmqd/sections/atuoj.md";const c=n=>({components:n,render:o});export{a as VueComponent,c as VueComponentWith,u as attributes,i as html,m as markdown};
