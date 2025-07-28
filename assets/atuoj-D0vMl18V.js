66942c499d9cb49f84447536baf98c730c65b216
africalib <africalibrary21@gmail.com>
2025-07-29 00:15:29 +0900

    published

---
+++ assets/atuoj-D0vMl18V.js
@@ -0,0 +1,17 @@
+import{c as s,o as e,b as r,e as t,F as n}from"./index-81nTAQqO.js";const c={},b=`<img src="/assets/sections/atuoj/hello.svg" class="w-50" alt="인사" />
+<p>아프리카에 도서관을 짓는 것이 꿈인 개발자 최진입니다.<br>
+언젠가 아프리카의 어떤 지역에서 도서관을 건축하고 운영하면서 아이들을 교육하고 양육하는 일을 하는 비전이 있습니다.</p>
+<p>그리고 유튜브의 <a href="https://www.youtube.com/@africalibrary21" target="_blank">&lt;아프리카도서관&gt;</a>이라는 채널을 운영하고 있습니다.<br>
+이 채널을 통해 프로그래밍과 관련된 내용을 공유하고 있습니다. 처음에는 스스로 공부하기 위해서 만든 유튜브 채널이었는데요, 신기하게도 많은 분들이 봐주셔서 지금까지 잘 성장해오고 있습니다.</p>
+<p>본 강의나 유튜브 등 문의사항이 있으시면 이메일 <a href="mailto:africalibrary21@gmail.com">africalibrary21@gmail.com</a>로 문의해주세요.<br>
+감사합니다.</p>
+`,u=`<img src="/assets/sections/atuoj/hello.svg" class="w-50" alt="인사" />\r
+\r
+아프리카에 도서관을 짓는 것이 꿈인 개발자 최진입니다.  \r
+언젠가 아프리카의 어떤 지역에서 도서관을 건축하고 운영하면서 아이들을 교육하고 양육하는 일을 하는 비전이 있습니다.\r
+\r
+그리고 유튜브의 <a href="https://www.youtube.com/@africalibrary21" target="_blank"><아프리카도서관></a>이라는 채널을 운영하고 있습니다.  \r
+이 채널을 통해 프로그래밍과 관련된 내용을 공유하고 있습니다. 처음에는 스스로 공부하기 위해서 만든 유튜브 채널이었는데요, 신기하게도 많은 분들이 봐주셔서 지금까지 잘 성장해오고 있습니다.\r
+\r
+본 강의나 유튜브 등 문의사항이 있으시면 이메일 <a href="mailto:africalibrary21@gmail.com">africalibrary21@gmail.com</a>로 문의해주세요.  \r
+감사합니다.`;function o(l,a){return e(),s(n,null,[a[0]||(a[0]=r("img",{src:"/assets/sections/atuoj/hello.svg",class:"w-50",alt:"인사"},null,-1)),a[1]||(a[1]=r("p",null,[t("아프리카에 도서관을 짓는 것이 꿈인 개발자 최진입니다."),r("br"),t(" 언젠가 아프리카의 어떤 지역에서 도서관을 건축하고 운영하면서 아이들을 교육하고 양육하는 일을 하는 비전이 있습니다.")],-1)),a[2]||(a[2]=r("p",null,[t("그리고 유튜브의 "),r("a",{href:"https://www.youtube.com/@africalibrary21",target:"_blank"},"<아프리카도서관>"),t("이라는 채널을 운영하고 있습니다."),r("br"),t(" 이 채널을 통해 프로그래밍과 관련된 내용을 공유하고 있습니다. 처음에는 스스로 공부하기 위해서 만든 유튜브 채널이었는데요, 신기하게도 많은 분들이 봐주셔서 지금까지 잘 성장해오고 있습니다.")],-1)),a[3]||(a[3]=r("p",null,[t("본 강의나 유튜브 등 문의사항이 있으시면 이메일 "),r("a",{href:"mailto:africalibrary21@gmail.com"},"africalibrary21@gmail.com"),t("로 문의해주세요."),r("br"),t(" 감사합니다.")],-1))],64)}const i={render:o};i.__hmrId="C:/J/repositories/africalib/src/data/courses/qkmqd/sections/atuoj.md";const f=l=>({components:l,render:o});export{i as VueComponent,f as VueComponentWith,c as attributes,b as html,u as markdown};
