c74d6e98d9600db74bd2056d045b8d0debd8a02a
africalib <africalibrary21@gmail.com>
2025-07-31 00:54:58 +0900

    published

--- assets/Login-B-G-iCH_.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./Soon-CMCZzS5H.js";import{f as s,c as t,b as o,d as a,o as c,_ as n}from"./index-DuMQEsMD.js";const _={class:"login"},r={class:"container"},d={class:"wrapper"},i=s({__name:"Login",setup(p){return(f,l)=>(c(),t("div",_,[o("div",r,[o("div",d,[a(e)])])]))}}),u=n(i,[["__scopeId","data-v-d48f2562"]]);export{u as default};
