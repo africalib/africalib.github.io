d9f920b0f8e9cf2f1ce8cbf9b4d92145dd630d96
africalib <africalibrary21@gmail.com>
2025-08-05 23:21:14 +0900

    published

---
+++ assets/mrmob-CouGhi48.js
@@ -0,0 +1,83 @@
+import{a}from"./index-Bl1Z_kCC.js";const o={},e=`<div>내 컴퓨터에 있는 사진 파일을 보려면 어떻게 해야 할까요? 다음과 같이 하면 될 것입니다.</div>
+<br />
+<figure>
+    <img src="/assets/courses/qkmqd/koadr/mrmob/steps1.svg" alt="내 컴퓨터에 있는 사진 파일을 보는 방법" class="w-100" />
+    <figcaption>
+        <span>내 컴퓨터에 있는 사진 파일을 보는 방법</span>
+        <a data-bs-toggle="popover">[1]</a>
+    </figcaption>
+</figure>
+<br />
+<div>
+    아주 쉽죠?
+    그런데 친구의 컴퓨터에 들어 있는 사진 파일을 보려면 어떻게 해야 할까요?
+    친구 집에 찾아가서 물어보고 컴퓨터를 켜서 사진 파일을 열어보면 될 것입니다
+</div>
+<br />
+<figure>
+    <img src="/assets/courses/qkmqd/koadr/mrmob/steps2.svg" alt="친구 컴퓨터에 있는 사진 파일을 보는 방법" class="w-100" />
+    <figcaption>
+        <span>친구 컴퓨터에 있는 사진 파일을 보는 방법</span>
+        <a data-bs-toggle="popover">[2]</a>
+    </figcaption>
+</figure>
+<br />
+<div>
+    하지만 매번 이렇게 친구 집에 찾아가기란 번거로운 일입니다.
+    그래서 이 일을 컴퓨터에게 시키면 어떨까요?
+    내 컴퓨터가 친구의 컴퓨터에게 파일을 요청하고 받는 것입니다. 이것이 가능한 것은 나와 내 친구의 컴퓨터를 포함해 전 세계의 수많은 컴퓨터들이 네트워크로 연결돼 있기 때문입니다.
+    그리고 이 거대한 네트워크를 가리켜 인터넷(Internet)이라고 합니다.
+</div>
+<br />
+<figure>
+    <img src="/assets/courses/qkmqd/koadr/mrmob/internet.svg" alt="거대한 네트워크, 인터넷" class="w-100" />
+    <figcaption>
+        <span>거대한 네트워크, 인터넷</span>
+        <a data-bs-toggle="popover">[3]</a>
+    </figcaption>
+</figure>
+<br />
+<div>
+    이렇게 해서 전 세계에서 사용하는 거대한 네트워크인 인터넷에 대해 알아봤습니다.
+</div>`,g=`<div>내 컴퓨터에 있는 사진 파일을 보려면 어떻게 해야 할까요? 다음과 같이 하면 될 것입니다.</div>\r
+<br />\r
+<figure>\r
+    <img src="/assets/courses/qkmqd/koadr/mrmob/steps1.svg" alt="내 컴퓨터에 있는 사진 파일을 보는 방법" class="w-100" />\r
+    <figcaption>\r
+        <span>내 컴퓨터에 있는 사진 파일을 보는 방법</span>\r
+        <a data-bs-toggle="popover">[1]</a>\r
+    </figcaption>\r
+</figure>\r
+<br />\r
+<div>\r
+    아주 쉽죠?\r
+    그런데 친구의 컴퓨터에 들어 있는 사진 파일을 보려면 어떻게 해야 할까요?\r
+    친구 집에 찾아가서 물어보고 컴퓨터를 켜서 사진 파일을 열어보면 될 것입니다\r
+</div>\r
+<br />\r
+<figure>\r
+    <img src="/assets/courses/qkmqd/koadr/mrmob/steps2.svg" alt="친구 컴퓨터에 있는 사진 파일을 보는 방법" class="w-100" />\r
+    <figcaption>\r
+        <span>친구 컴퓨터에 있는 사진 파일을 보는 방법</span>\r
+        <a data-bs-toggle="popover">[2]</a>\r
+    </figcaption>\r
+</figure>\r
+<br />\r
+<div>\r
+    하지만 매번 이렇게 친구 집에 찾아가기란 번거로운 일입니다.\r
+    그래서 이 일을 컴퓨터에게 시키면 어떨까요?\r
+    내 컴퓨터가 친구의 컴퓨터에게 파일을 요청하고 받는 것입니다. 이것이 가능한 것은 나와 내 친구의 컴퓨터를 포함해 전 세계의 수많은 컴퓨터들이 네트워크로 연결돼 있기 때문입니다.\r
+    그리고 이 거대한 네트워크를 가리켜 인터넷(Internet)이라고 합니다.\r
+</div>\r
+<br />\r
+<figure>\r
+    <img src="/assets/courses/qkmqd/koadr/mrmob/internet.svg" alt="거대한 네트워크, 인터넷" class="w-100" />\r
+    <figcaption>\r
+        <span>거대한 네트워크, 인터넷</span>\r
+        <a data-bs-toggle="popover">[3]</a>\r
+    </figcaption>\r
+</figure>\r
+<br />\r
+<div>\r
+    이렇게 해서 전 세계에서 사용하는 거대한 네트워크인 인터넷에 대해 알아봤습니다.\r
+</div>`;function s(r,n){return n[0]||(n[0]=a('<div>내 컴퓨터에 있는 사진 파일을 보려면 어떻게 해야 할까요? 다음과 같이 하면 될 것입니다.</div><br><figure><img src="/assets/courses/qkmqd/koadr/mrmob/steps1.svg" alt="내 컴퓨터에 있는 사진 파일을 보는 방법" class="w-100"><figcaption><span>내 컴퓨터에 있는 사진 파일을 보는 방법</span><a data-bs-toggle="popover">[1]</a></figcaption></figure><br><div> 아주 쉽죠? 그런데 친구의 컴퓨터에 들어 있는 사진 파일을 보려면 어떻게 해야 할까요? 친구 집에 찾아가서 물어보고 컴퓨터를 켜서 사진 파일을 열어보면 될 것입니다 </div><br><figure><img src="/assets/courses/qkmqd/koadr/mrmob/steps2.svg" alt="친구 컴퓨터에 있는 사진 파일을 보는 방법" class="w-100"><figcaption><span>친구 컴퓨터에 있는 사진 파일을 보는 방법</span><a data-bs-toggle="popover">[2]</a></figcaption></figure><br><div> 하지만 매번 이렇게 친구 집에 찾아가기란 번거로운 일입니다. 그래서 이 일을 컴퓨터에게 시키면 어떨까요? 내 컴퓨터가 친구의 컴퓨터에게 파일을 요청하고 받는 것입니다. 이것이 가능한 것은 나와 내 친구의 컴퓨터를 포함해 전 세계의 수많은 컴퓨터들이 네트워크로 연결돼 있기 때문입니다. 그리고 이 거대한 네트워크를 가리켜 인터넷(Internet)이라고 합니다. </div><br><figure><img src="/assets/courses/qkmqd/koadr/mrmob/internet.svg" alt="거대한 네트워크, 인터넷" class="w-100"><figcaption><span>거대한 네트워크, 인터넷</span><a data-bs-toggle="popover">[3]</a></figcaption></figure><br><div> 이렇게 해서 전 세계에서 사용하는 거대한 네트워크인 인터넷에 대해 알아봤습니다. </div>',13))}const i={render:s};i.__hmrId="C:/J/repositories/africalib/src/data/courses/qkmqd/koadr/mrmob.md";const p=r=>({components:r,render:s});export{i as VueComponent,p as VueComponentWith,o as attributes,e as html,g as markdown};
