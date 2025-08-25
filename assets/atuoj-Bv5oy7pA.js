2079dd5703ca0964bd379763c030f8b33990b9eb
africalib <africalibrary21@gmail.com>
2025-08-26 00:16:41 +0900

    published

---
+++ assets/atuoj-Bv5oy7pA.js
@@ -0,0 +1,137 @@
+import{a as i}from"./index-CLoWHW-L.js";const p={},o=`<figure class="slide-item">
+    <img src="/assets/courses/qkmqd/dgjni/atuoj/hello.svg" alt="인사" class="mh-400px" />
+    <figcaption>
+        <span>강사</span>
+        <b>아프리카도서관</b>
+        <span>소개</span>
+        <span data-bs-toggle="popover"></span>
+    </figcaption>
+</figure>
+<br />
+<div class="slide-text">
+    안녕하세요? 백엔드, 프론트엔드, 인프라 등 다양한 소프트웨어 분야에 관심이 많은 개발자 최진입니다.
+</div>
+<br />
+<figure class="slide-item">
+    <img src="/assets/courses/qkmqd/dgjni/atuoj/youtube.png" alt="유튜브 채널" class="mh-400px lined" />
+    <figcaption>
+        <b>유튜브</b>
+        <span>채널 화면</span>
+        <span data-bs-toggle="popover"></span>
+    </figcaption>
+</figure>
+<br />
+<div class="slide-text">
+    그리고 유튜브의 <a href="https://www.youtube.com/@africalibrary21" target="_blank">아프리카도서관</a>이라는 채널을 운영하고 있습니다.
+    이 채널을 통해 프로그래밍과 관련된 내용을 공유하고 있습니다. 처음에는 스스로 공부하기 위해서 만든 유튜브 채널이었는데요, 감사하게도 많은 분들이 좋게 봐주셔서 지금까지 잘 성장해오고 있습니다.
+</div>
+<br />
+<figure class="slide-item">
+    <img src="/assets/courses/qkmqd/dgjni/atuoj/book.jpg" alt="Vue 3와 스프링 부트로 시작하는 웹 개발 철저 입문" class="mh-500px lined" />
+    <figcaption>
+        <b>도서</b>
+        <span>: Vue3와 스프링부트로 시작하는 웹 개발 철저 입문</span>
+        <span data-bs-toggle="popover"></span>
+    </figcaption>
+</figure>
+<br />
+<div class="slide-text">
+    그리고 최근에 Vue 3와 스프링 부트를 다룬 입문서를 집필하였습니다. 이번 강의와 동일한 주제를 많이 다루고 있기 때문에 이 책을 함께 보시는 것도 도움이 되실 것 같습니다.
+</div>
+<br />
+<figure class="slide-item">
+    <img src="/assets/courses/qkmqd/dgjni/atuoj/ubuntu.svg" alt="우분투, 네가 있어 내가 있다." class=mh-150px />
+    <figcaption>
+        <b>우분투</b><span>: 네가 있어 내가 있다</span>
+        <span data-bs-toggle="popover"></span>
+    </figcaption>
+</figure>
+<br />
+<div class="slide-text">
+    그리고 언젠가 아프리카에 도서관을 짓고 싶은 꿈이 있습니다.
+    "우분투"라는 말이 있는데요, 이 말은 "네가 있어 내가 있다"라는 뜻의 남아프리카공화국의 반투어입니다.
+    이 말은 나 스스로 존재하는 것이 아니라 타인과의 함께 존재한다는 가치를 담고 있습니다.
+    이러한 가치관을 바탕으로 아프리카에 도서관을 짓고 아이들을 교육하고 싶습니다.
+</div>
+<br />
+<figure class="slide-item">
+    <img src="/assets/courses/qkmqd/dgjni/atuoj/email.svg" alt="이메일 주소" class="mh-400px" />
+    <figcaption>
+        <b>이메일</b>
+        <span>주소</span>
+    </figcaption>
+</figure>
+<br />
+<div>
+    <span class="slide-text">본 강의나 유튜브 등 문의사항이 있으시면 이메일로 문의해주시기 바랍니다.</span>
+    <br />
+    <span class="slide-text">이렇게 해서 프롤로그에 대해 살펴봤습니다.</span>
+</div>`,r=`<figure class="slide-item">
+    <img src="/assets/courses/qkmqd/dgjni/atuoj/hello.svg" alt="인사" class="mh-400px" />
+    <figcaption>
+        <span>강사</span>
+        <b>아프리카도서관</b>
+        <span>소개</span>
+        <span data-bs-toggle="popover"></span>
+    </figcaption>
+</figure>
+<br />
+<div class="slide-text">
+    안녕하세요? 백엔드, 프론트엔드, 인프라 등 다양한 소프트웨어 분야에 관심이 많은 개발자 최진입니다.
+</div>
+<br />
+<figure class="slide-item">
+    <img src="/assets/courses/qkmqd/dgjni/atuoj/youtube.png" alt="유튜브 채널" class="mh-400px lined" />
+    <figcaption>
+        <b>유튜브</b>
+        <span>채널 화면</span>
+        <span data-bs-toggle="popover"></span>
+    </figcaption>
+</figure>
+<br />
+<div class="slide-text">
+    그리고 유튜브의 <a href="https://www.youtube.com/@africalibrary21" target="_blank">아프리카도서관</a>이라는 채널을 운영하고 있습니다.
+    이 채널을 통해 프로그래밍과 관련된 내용을 공유하고 있습니다. 처음에는 스스로 공부하기 위해서 만든 유튜브 채널이었는데요, 감사하게도 많은 분들이 좋게 봐주셔서 지금까지 잘 성장해오고 있습니다.
+</div>
+<br />
+<figure class="slide-item">
+    <img src="/assets/courses/qkmqd/dgjni/atuoj/book.jpg" alt="Vue 3와 스프링 부트로 시작하는 웹 개발 철저 입문" class="mh-500px lined" />
+    <figcaption>
+        <b>도서</b>
+        <span>: Vue3와 스프링부트로 시작하는 웹 개발 철저 입문</span>
+        <span data-bs-toggle="popover"></span>
+    </figcaption>
+</figure>
+<br />
+<div class="slide-text">
+    그리고 최근에 Vue 3와 스프링 부트를 다룬 입문서를 집필하였습니다. 이번 강의와 동일한 주제를 많이 다루고 있기 때문에 이 책을 함께 보시는 것도 도움이 되실 것 같습니다.
+</div>
+<br />
+<figure class="slide-item">
+    <img src="/assets/courses/qkmqd/dgjni/atuoj/ubuntu.svg" alt="우분투, 네가 있어 내가 있다." class=mh-150px />
+    <figcaption>
+        <b>우분투</b><span>: 네가 있어 내가 있다</span>
+        <span data-bs-toggle="popover"></span>
+    </figcaption>
+</figure>
+<br />
+<div class="slide-text">
+    그리고 언젠가 아프리카에 도서관을 짓고 싶은 꿈이 있습니다.
+    "우분투"라는 말이 있는데요, 이 말은 "네가 있어 내가 있다"라는 뜻의 남아프리카공화국의 반투어입니다.
+    이 말은 나 스스로 존재하는 것이 아니라 타인과의 함께 존재한다는 가치를 담고 있습니다.
+    이러한 가치관을 바탕으로 아프리카에 도서관을 짓고 아이들을 교육하고 싶습니다.
+</div>
+<br />
+<figure class="slide-item">
+    <img src="/assets/courses/qkmqd/dgjni/atuoj/email.svg" alt="이메일 주소" class="mh-400px" />
+    <figcaption>
+        <b>이메일</b>
+        <span>주소</span>
+    </figcaption>
+</figure>
+<br />
+<div>
+    <span class="slide-text">본 강의나 유튜브 등 문의사항이 있으시면 이메일로 문의해주시기 바랍니다.</span>
+    <br />
+    <span class="slide-text">이렇게 해서 프롤로그에 대해 살펴봤습니다.</span>
+</div>`;function a(s,n){return n[0]||(n[0]=i('<figure class="slide-item"><img src="/assets/courses/qkmqd/dgjni/atuoj/hello.svg" alt="인사" class="mh-400px"><figcaption><span>강사</span><b>아프리카도서관</b><span>소개</span><span data-bs-toggle="popover"></span></figcaption></figure><br><div class="slide-text"> 안녕하세요? 백엔드, 프론트엔드, 인프라 등 다양한 소프트웨어 분야에 관심이 많은 개발자 최진입니다. </div><br><figure class="slide-item"><img src="/assets/courses/qkmqd/dgjni/atuoj/youtube.png" alt="유튜브 채널" class="mh-400px lined"><figcaption><b>유튜브</b><span>채널 화면</span><span data-bs-toggle="popover"></span></figcaption></figure><br><div class="slide-text"> 그리고 유튜브의 <a href="https://www.youtube.com/@africalibrary21" target="_blank">아프리카도서관</a>이라는 채널을 운영하고 있습니다. 이 채널을 통해 프로그래밍과 관련된 내용을 공유하고 있습니다. 처음에는 스스로 공부하기 위해서 만든 유튜브 채널이었는데요, 감사하게도 많은 분들이 좋게 봐주셔서 지금까지 잘 성장해오고 있습니다. </div><br><figure class="slide-item"><img src="/assets/courses/qkmqd/dgjni/atuoj/book.jpg" alt="Vue 3와 스프링 부트로 시작하는 웹 개발 철저 입문" class="mh-500px lined"><figcaption><b>도서</b><span>: Vue3와 스프링부트로 시작하는 웹 개발 철저 입문</span><span data-bs-toggle="popover"></span></figcaption></figure><br><div class="slide-text"> 그리고 최근에 Vue 3와 스프링 부트를 다룬 입문서를 집필하였습니다. 이번 강의와 동일한 주제를 많이 다루고 있기 때문에 이 책을 함께 보시는 것도 도움이 되실 것 같습니다. </div><br><figure class="slide-item"><img src="/assets/courses/qkmqd/dgjni/atuoj/ubuntu.svg" alt="우분투, 네가 있어 내가 있다." class="mh-150px"><figcaption><b>우분투</b><span>: 네가 있어 내가 있다</span><span data-bs-toggle="popover"></span></figcaption></figure><br><div class="slide-text"> 그리고 언젠가 아프리카에 도서관을 짓고 싶은 꿈이 있습니다. &quot;우분투&quot;라는 말이 있는데요, 이 말은 &quot;네가 있어 내가 있다&quot;라는 뜻의 남아프리카공화국의 반투어입니다. 이 말은 나 스스로 존재하는 것이 아니라 타인과의 함께 존재한다는 가치를 담고 있습니다. 이러한 가치관을 바탕으로 아프리카에 도서관을 짓고 아이들을 교육하고 싶습니다. </div><br><figure class="slide-item"><img src="/assets/courses/qkmqd/dgjni/atuoj/email.svg" alt="이메일 주소" class="mh-400px"><figcaption><b>이메일</b><span>주소</span></figcaption></figure><br><div><span class="slide-text">본 강의나 유튜브 등 문의사항이 있으시면 이메일로 문의해주시기 바랍니다.</span><br><span class="slide-text">이렇게 해서 프롤로그에 대해 살펴봤습니다.</span></div>',19))}const e={render:a};e.__hmrId="C:/J/repositories/africalib/src/data/courses/qkmqd/dgjni/atuoj.md";const g=s=>({components:s,render:a});export{e as VueComponent,g as VueComponentWith,p as attributes,o as html,r as markdown};
---
+++ assets/burtr-9SFWz6uU.js
@@ -0,0 +1,2 @@
+import{c as o,o as n}from"./index-CLoWHW-L.js";const a={},p=`<p>준비 중</p>
+`,m="준비 중";function e(t,c){return n(),o("p",null,"준비 중")}const r={render:e};r.__hmrId="C:/J/repositories/africalib/src/data/posts/burtr.md";const i=t=>({components:t,render:e});export{r as VueComponent,i as VueComponentWith,a as attributes,p as html,m as markdown};
