d9dbfa590891dab7b7d2aba2fcc4cfc041787e23
africalib <africalibrary21@gmail.com>
2025-08-26 00:50:44 +0900

    published

---
+++ assets/zywlu-Lbkzeruy.js
@@ -0,0 +1,47 @@
+import{a as i}from"./index-BBE4IsrR.js";const t={},d=`<figure class="slide-item">
+    <img src="/assets/courses/qkmqd/hufwn/zywlu/install.vscode.svg" alt="Visual Studio Code 설치" class="mh-400px" />
+    <figcaption>
+        <span>macOS에</span>
+        <b>Visual Studio Code</b>
+        <span>설치하기</span>
+        <span data-bs-toggle="popover"></span>
+    </figcaption>
+</figure>
+<br />
+<div class="slide-text">
+    <div class="mb-3">
+        <b>다음 순서에 따라 진행하시면 됩니다.</b>
+    </div>
+    1. 구글 크롬 브라우저를 실행합니다.
+    <br />
+    2. 검색 창에 "vscode"를 검색합니다.
+    <br />
+    3. 검색 결과 상단에 있는 항목을 클릭하여 웹 사이트에 접속합니다.
+    <br />
+    4. 화면 중간에 있는 [Download for Mac] 버튼을 클릭하여 설치 파일을 다운로드합니다.
+    <br />
+    5. 설치 파일을 실행하여 구글 크롬을 설치합니다.
+</div>`,r=`<figure class="slide-item">
+    <img src="/assets/courses/qkmqd/hufwn/zywlu/install.vscode.svg" alt="Visual Studio Code 설치" class="mh-400px" />
+    <figcaption>
+        <span>macOS에</span>
+        <b>Visual Studio Code</b>
+        <span>설치하기</span>
+        <span data-bs-toggle="popover"></span>
+    </figcaption>
+</figure>
+<br />
+<div class="slide-text">
+    <div class="mb-3">
+        <b>다음 순서에 따라 진행하시면 됩니다.</b>
+    </div>
+    1. 구글 크롬 브라우저를 실행합니다.
+    <br />
+    2. 검색 창에 "vscode"를 검색합니다.
+    <br />
+    3. 검색 결과 상단에 있는 항목을 클릭하여 웹 사이트에 접속합니다.
+    <br />
+    4. 화면 중간에 있는 [Download for Mac] 버튼을 클릭하여 설치 파일을 다운로드합니다.
+    <br />
+    5. 설치 파일을 실행하여 구글 크롬을 설치합니다.
+</div>`;function a(s,n){return n[0]||(n[0]=i('<figure class="slide-item"><img src="/assets/courses/qkmqd/hufwn/zywlu/install.vscode.svg" alt="Visual Studio Code 설치" class="mh-400px"><figcaption><span>macOS에</span><b>Visual Studio Code</b><span>설치하기</span><span data-bs-toggle="popover"></span></figcaption></figure><br><div class="slide-text"><div class="mb-3"><b>다음 순서에 따라 진행하시면 됩니다.</b></div> 1. 구글 크롬 브라우저를 실행합니다. <br> 2. 검색 창에 &quot;vscode&quot;를 검색합니다. <br> 3. 검색 결과 상단에 있는 항목을 클릭하여 웹 사이트에 접속합니다. <br> 4. 화면 중간에 있는 [Download for Mac] 버튼을 클릭하여 설치 파일을 다운로드합니다. <br> 5. 설치 파일을 실행하여 구글 크롬을 설치합니다. </div>',3))}const o={render:a};o.__hmrId="C:/J/repositories/africalib/src/data/courses/qkmqd/hufwn/zywlu.md";const c=s=>({components:s,render:a});export{o as VueComponent,c as VueComponentWith,t as attributes,d as html,r as markdown};
