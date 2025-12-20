d88adfe30d6f30521112dabc3b0473dc123a92a5
africalib <africalibrary21@gmail.com>
2025-12-20 23:03:02 +0900

    published

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -1,7 +1,7 @@
 window.onIframeLoad = ($iframe) => {
     try {
         const doc = $iframe.contentDocument || $iframe.contentWindow.document;
-        $iframe.style.height = (doc.body.scrollHeight + 1) + 'px';
+        $iframe.style.height = (doc.body.scrollHeight + 2) + 'px';
     } catch (e) {
         console.error(e);
     }
